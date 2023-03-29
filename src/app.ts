597b4427cdefcc1504ddf1f29f1bf8c48fa4648f
Eivind Eriksen <mail@eivinderiksen.com>
2023-03-29 11:18:21 +0200

    Removed unused.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express, {
     Application,
     Express,
+    NextFunction,
     Request,
     Response,
-    NextFunction,
 } from "express";
 import { htmlToPdf } from "./service/html-to-pdf.service";
-import cluster from "cluster";
 
 const createServer = (): Application => {
     const app: Express = express();
@@ -15,7 +14,7 @@ const createServer = (): Application => {
     app.get("/html-to-pdf", htmlToPdf);
 
     // Error handler
-    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    app.use((err: Error, req: Request, res: Response) => {
         console.error(err.stack);
         res.status(500).send(`Error: ${err.message}`);
     });
